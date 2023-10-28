7844352f7b9bcc247814a159f7e049fc9b823ca6
Novo1999 <neptune.navo@gmail.com>
2023-10-28 20:16:14 +0600

    update

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import 'express-async-errors'
 import express from 'express'
+import { Request, Response } from 'express'
 import mongoose from 'mongoose'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
@@ -10,6 +11,7 @@ import mongoSanitize from 'express-mongo-sanitize'
 import authRouter from './routes/auth.js'
 import productRouter from './routes/products.ts'
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.ts'
+import { StatusCodes } from 'http-status-codes'
 
 dotenv.config()
 
@@ -17,8 +19,6 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-
-const port = process.env.PORT || 6700
 
 app.use(morgan('dev'))
 app.use(cors())
@@ -28,8 +28,17 @@ app.use(mongoSanitize())
 app.use('/api/e-commerce/auth', authRouter)
 app.use('/api/e-commerce/products', productRouter)
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('This server works')
+})
+
+app.use('*', (req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json({ msg: 'not found' })
+})
+
 app.use(errorHandlerMiddleware)
 
+const port = process.env.PORT || 6700
 try {
   await mongoose.connect(process.env.MONGO_URL!)
   app.listen(port, () => {
